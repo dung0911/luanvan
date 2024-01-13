b085ae5b73ba79002e5c4ee8bd39fbb89042437c
dung <73698704+dung0911@users.noreply.github.com>
2024-01-13 21:06:50 +0700

    Update config.php

--- backend/Config/config.php
+++ backend/Config/config.php
@@ -1,15 +1,17 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "localhost";
-$configDB["database"]	= "shopquanao";
-$configDB["username"] 	= "root";
-$configDB["password"] 	= "";
-define("HOST", "localhost");
-define("DB_NAME", "shopquanao");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define('ROOT', dirname(dirname(__FILE__) ) );
+$configDB[ 'host' ] 		 = 'http://shopluanvan.x10.mx/';
+$configDB[ 'database' ]	 = 'diyadblz_lvtn';
+$configDB[ 'username' ] 	 = 'diyadblz_lvtn';
+$configDB[ 'password' ] 	 = '123456';
 
+define( 'HOST', $configDB[ 'host' ] );
+define( 'DB_NAME', $configDB[ 'database' ] );
+define( 'DB_USER', $configDB[ 'username' ] );
+define( 'DB_PASS', $configDB[ 'password' ] );
+define( 'ROOT', dirname( dirname( __FILE__ ) ) );
+define( 'BASE_URL', 'http://'.$_SERVER[ 'SERVER_NAME' ].'/lab/' );
+define( 'TOP_SELLER', 3 );
+define( 'STRLEN_MO_TA', 60 );
+define( 'KHOA_HOC_MOT_TRANG', 12 );
 ?>
-
-
