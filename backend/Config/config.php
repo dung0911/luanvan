16a35c7231ba101fd85639a6f45295a90cb17537
dung <73698704+dung0911@users.noreply.github.com>
2024-01-13 20:52:08 +0700

    Update config.php

--- backend/Config/config.php
+++ backend/Config/config.php
@@ -9,3 +9,9 @@ define("DB_NAME", "shopquanao");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define('ROOT', dirname(dirname(__FILE__) ) );
+//Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
+// define("BASE_URL", "http://".$_SERVER['SERVER_NAME']."/lab/");//dia chi website
+// define("TOP_SELLER", 3);
+// define("STRLEN_MO_TA", 60);
+// define("KHOA_HOC_MOT_TRANG", 12);
+?>
